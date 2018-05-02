6f2585d39dd7a5c9baa9400e2db17e5011ffb996
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-05-02 15:48:09 -0500

    properly scrapes all menu items

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # alexa_dininghall
+
+# TO DO
+* function that picks out the day so we can find the appropriate daily menu
--- scrape.py
+++ scrape.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup as bs
 import urllib
 
-url="https://menus.sodexomyway.com/BiteMenu/Menu?menuId=1164&locationId=10380002&whereami=http://northwestern.sodexomyway.com/dining-near-me/foster-walker-west"
+# storing some information here
+# looks like the menu is divided by day like this: menuid-#-day
+
+url="https://menus.sodexomyway.com/BiteMenu/Menu?menuId=337&locationId=10380001&whereami=http://northwestern.sodexomyway.com/dining-near-me/sargent"
 
 soup = bs(urllib.urlopen(url), 'html.parser')
 # for link in soup.findAll('a'):
@@ -9,6 +12,19 @@ soup = bs(urllib.urlopen(url), 'html.parser')
 # for food in soup.find_all("div", class_="bite-menu-item"):
 #     print food.
 
-for item in soup.select(".bite-menu-item button"):
-    if item != None or item.string != "":
+categories = []
+for item in soup.select("#menuid-2-day .accordion-copy"):
+    categories.append(item.string)
+
+food = []
+#for item in soup.select(".bite-menu-item button"):
+for item in soup.select(".get-nutrition"):
+    if item.string == None:
+        # item.string returns None because these specific menu entries have images with them
+        print item.contents[0]
+    else:
         print item.string
+    #food.append(item.string)
+
+#print food
+
